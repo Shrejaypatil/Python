17b0ceec89fb864f6f9c311f6f025c566333ae11
Shrejay Patil <96781380+Shrejaypatil@users.noreply.github.com>
2025-08-17 18:20:12 +0530

    Update decorator.py

--- Automation/decorator.py
+++ Automation/decorator.py
@@ -5,6 +5,14 @@ decorators are nothing but functions that modify other functions
 similar to event decorators in real life, we can also call it as
 wrapper functions.
 
+concepts covered:
+
+nested func
+func as parameters
+decorators
+@wrapper functions
+decorator chaining
+
 '''
 
 def event_decorator(func):
@@ -23,4 +31,35 @@ stage()
 # manager = event_decorator(stage)
 
 # manager()
+
+#decorator chaining
+
+'''
+we can use multiple decorators, the innner most will run first
+'''
+
+
+def star(func):
+    def innner():
+        print("*"*20)
+        func()
+        print("*"*20)
+        
+    return innner
     
+
+def dollar(func):
+    def inner():
+        print("$"*20)
+        func()
+        print("$"*20)
+        
+    return inner
+    
+    
+@star
+@dollar
+def printer():
+    print("Hello world!")
+
+printer()
